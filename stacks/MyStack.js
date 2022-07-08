05cd0c5f2199ea8fc4563c7de909675798b285b9
JagjiwanChimkar <jagjiwanchimkar333@gmail.com>
2022-07-08 19:12:02 +0530

    Adding a storage stack

--- stacks/MyStack.js
+++ stacks/MyStack.js
@@ -1,12 +1,15 @@
 import { Api } from "@serverless-stack/resources";
 
 export function MyStack({ stack }) {
-  const api = new Api(stack, "api", {
+  // Create an HTTP API
+  const api = new Api(stack, "Api", {
     routes: {
       "GET /": "functions/lambda.handler",
     },
   });
+
+  // Show the endpoint in the output
   stack.addOutputs({
-    ApiEndpoint: api.url
+    ApiEndpoint: api.url,
   });
 }
